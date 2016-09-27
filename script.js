3578df7fe36be8e9109638843761a0abfa20ef6e
FORESTIERK <ForestierCODAGE@outlook.fr>
2016-09-27 17:01:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function parag1(){
 		document.getElementById('parag1').style.border = "dotted red";
 		document.getElementById('parag1').style.borderWidth = "10px";
 		document.getElementById('parag1').style.padding = "5px";
-		document.getElementById('fond').style.background ="gray";
+		document.getElementById('fond').style.backgroundImage ="url('img.jpg')";
 		document.getElementById('bouton').style.color = "red";
 		document.getElementById('bouton').style.background = "black";
 		document.getElementById('bouton').style.border = "solid red";
 		document.getElementById('bouton').style.borderWidth = "2px";
-		document.getElementById('fond').src = "img.jpg";
+		alert('JE T\'AI EU !!');
 }
