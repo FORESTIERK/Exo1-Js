18ffca10f39f4613170578b95bbc689b63f72024
FORESTIERK <ForestierCODAGE@outlook.fr>
2016-09-27 15:55:59 +0200

    Js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* votre fonction ici*/
+function parag1(){
+		document.getElementById('parag1').style.background = "black";
+		document.getElementById('parag1').style.color = "white";
+		document.getElementById('parag1').style.border = "dotted red";
+		document.getElementById('parag1').style.borderWidth = "10px";
+		document.getElementById('parag1').style.padding = "5px";
+		document.getElementById('fond').style.background ="gray";
+		document.getElementById('bouton').style.color = "red";
+		document.getElementById('bouton').style.background = "black";
+		document.getElementById('bouton').style.border = "solid red";
+		document.getElementById('bouton').style.borderWidth = "2px";
+		document.getElementById('fond').src = "img.jpg";
+}
